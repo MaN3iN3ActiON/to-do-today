80765ea76804e3a3ac83fe4e6eb0f712121054e9
MaN3iN3ActiON <ychnagasivareddy@gmail.com>
2018-09-26 21:05:44 +0530

    Add TaskInput.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import './App.css';
 import TaskList from './TaskList';
 import { TASKS } from './data';
+import TaskInput from './TaskInput';
 
 class App extends React.Component {
 	render() {
 		return (
 			<Fragment>
 				<h1>Tasks</h1>
+				<TaskInput />
 				<TaskList data={TASKS} />
 			</Fragment>
 		);
---
+++ src/TaskInput.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class TaskInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '' };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(event) {
+		const value = event.target.value;
+		this.setState({ value });
+	}
+	handleSubmit(event) {
+		event.preventDefault();
+		//things are still static so use alert
+		alert('A task was submitted: ' + this.state.value);
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Task:
+					<input
+						type="text"
+						value={this.state.value}
+						onChange={this.handleChange}
+					/>
+				</label>
+				<input type="submit" value="submit" />
+			</form>
+		);
+	}
+}
+
+export default TaskInput;
